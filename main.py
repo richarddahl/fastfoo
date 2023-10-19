37bc2d7f500f577fba28612c6ded496535108b0d
Richard Dahl <rdahl@cmplid.com>
2023-10-19 14:28:14 -0500

    begin UserFilterSchema definition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
 import os
 import django
+from django.core.paginator import Paginator
+from asgiref.sync import sync_to_async
+
+from pydantic import BaseModel
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -9,15 +13,25 @@ from fastapi.templating import Jinja2Templates
 
 
 sys.dont_write_bytecode = True
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foo.settings")
 django.setup()
 
+from foobar.models import Foo
+
+
+class FooSchema(BaseModel):
+    name: str
+    text: str | None = None
+
+
+class UserFilterSchema(BaseModel):
+    multiple: bool | None = None
+    element: str = "input"
+    children: list[]
+
+
 app = FastAPI()
-
-
-app.mount("/static",
-          StaticFiles(directory="static"),
-          name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -28,5 +42,13 @@ async def root(request: Request):
 
 
 @app.get("/api/foos")
-async def list_foos(request:Request):
-    pass
+def list_foos(request: Request) -> list[FooSchema]:
+    foos = Foo.objects.all()
+    p = Paginator(foos, 25)
+    f_list = [FooSchema(name=foo.name, text=foo.text) for foo in p.get_page(1)]
+    return f_list
+
+
+@app.get("/api/filters")
+def list_filters(request: Request) -> list[FilterSchema]:
+    return []
